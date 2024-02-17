6c7cdfee0c8cc4f3f8e3ffa283ea9bd0581a8a0c
HUMBLE <79673680+Lets-Pray@users.noreply.github.com>
2024-02-17 22:07:01 +0700

    Update service.sh
    Trying to change "model" and change to 6800mah

--- service.sh
+++ service.sh
@@ -1,10 +1,11 @@
 #!/system/bin/sh
 #
-sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/class/power_supply/bms/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/bms/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/class/power_supply/battery/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/battery/uevent
-echo 4000000 > /sys/class/power_supply/bms/charge_full
-echo 4000000 > /sys/class/power_supply/bms/charge_full_design
-echo 4000000 > /sys/class/power_supply/battery/charge_full
-echo 4000000 > /sys/class/power_supply/battery/charge_full_design
+sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=6800000/' /sys/class/power_supply/bms/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=6800000/' /sys/class/power_supply/bms/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=6800000/' /sys/class/power_supply/battery/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=6800000/' /sys/class/power_supply/battery/uevent
+sed -i 's/3292665_huaqin_ql1626cos_5000mah_averaged_masterslave_jan8th2018/EB-BM415ABY (Samsung M51)/' /sys/class/power_supply/battery/bms/battery_type
+echo 6800000 > /sys/class/power_supply/bms/charge_full
+echo 6800000 > /sys/class/power_supply/bms/charge_full_design
+echo 6800000 > /sys/class/power_supply/battery/charge_full
+echo 6800000 > /sys/class/power_supply/battery/charge_full_design
